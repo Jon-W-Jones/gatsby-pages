9bc5f30b5660a83c4380f26301b7893af61655e1
Jon Jones <jonwj100@gmail.com>
2020-01-22 22:08:03 -0600

    Create slugs for each post.

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,14 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const { createFilePath } = require("gatsby-source-filesystem")
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  //if node is a MarkDownRemark, then create a custom slug for file gen.
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode, basePath: "posts" })
+    createNodeField({ node, name: "slug", value: slug })
+  }
+}
