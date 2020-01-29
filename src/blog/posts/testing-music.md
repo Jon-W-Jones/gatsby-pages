04301f5c9003ad32601faef8cc015978142d6520
Jon Jones <jonwj100@gmail.com>
2020-01-28 22:05:45 -0600

    Played around with gatsby-image and gatsby-remark-images to see how to handle images within a gatsby site.

---
+++ src/blog/posts/testing-music.md
@@ -0,0 +1,10 @@
+---
+title: "Music Taste"
+date: "2020-01-28"
+---
+
+I was listening to some classic rock while learning about how to handle images in Gatsby, so I thought it was fairly appropriate that I include a photo, from Unsplash, depcting an album of The Doors as practice on how to use gatsby-remark-images.
+
+![The Doors Album](The-Doors.jpg)
+
+Hurray it seems that the gatsby plugin works pretty well! Now to figure out more about image customization so that more interesting and useful additions can be made.
--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,62 @@
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Logo from "../images/Jon-Logo.png"
+import style from "./header.module.scss"
+import Img from "gatsby-image"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const LOGO_QUERY = graphql`
+  query {
+    file(relativePath: { eq: "Jon-Logo.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 200) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+const Header = ({ siteTitle }) => {
+  const logo = useStaticQuery(LOGO_QUERY)
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <span className={style.titleRow}>
+          {/* Colors will change, this is a placeholder logo color... well the whole header is kind of a placeholder. */}
+          <Img
+            fluid={logo.file.childImageSharp.fluid}
+            alt="Jon Jones Logo"
+            src={Logo}
+            className={style.logo}
+          />
+          <h1 className={style.titleContainer}>
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+        </span>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
